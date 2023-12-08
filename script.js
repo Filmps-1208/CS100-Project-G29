c25724ce5672ab85ac61a4db05e12338d663a8ef
pornteera <pornteera.sao@dome.tu.ac.th>
2023-12-08 21:13:31 +0700

    k

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+function validateName() {
+  const fullnameInput = document.getElementById("fullname");
+  const names = fullnameInput.value.trim().split(" ");
+  const errorElement = document.getElementById("fullnameError");
+
+  if (names.length !== 2) {
+    errorElement.textContent = "Please enter both your Firstname and Lastname.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+function validateStudentID() {
+  const studentIDInput = document.getElementById("studentID");
+  const studentIDPattern = /^\d{10}$/;
+  const errorElement = document.getElementById("studentIDError");
+
+  if (!studentIDPattern.test(studentIDInput.value)) {
+    errorElement.textContent = "Please enter a 10-digit Student ID.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailPattern = /^.+@dome\.tu\.ac\.th$/;
+  const errorElement = document.getElementById("emailError");
+
+  if (!emailPattern.test(emailInput.value)) {
+    errorElement.textContent =
+      "Please provide a valid university email in the format 'xxx.yyy@dome.tu.ac.th'.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+///////////////////////////////////////////
+
 function showFormData() {
     // เข้าถึงข้อมูลที่ผู้ใช้กรอก
     const fullname = document.getElementById('fullname').value;
